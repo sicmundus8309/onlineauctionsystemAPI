2bc68cbfb87cf253a3d939e613746cc013ea632c
sicmundus8309 <139063689+sicmundus8309@users.noreply.github.com>
2024-03-29 21:33:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: "https://onlineauctionsystem-di6g.onrender.com", 
   credentials: true 
 }));
 
